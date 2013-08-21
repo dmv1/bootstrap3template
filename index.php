ef3ce14c78863669638a26073b8a0b7b7119ca4f
Don Voltz <donald.voltz@gmail.com>
2013-08-21 05:00:20 -0400

    bootstrap template initial files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
